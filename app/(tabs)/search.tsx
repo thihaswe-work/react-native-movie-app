07eedddbed9acc4e41c65de42e7fb6a4ea88d73f
thihaswe.work <thihaswe.work@gmail.com>
2025-07-03 16:05:50 +0630

    search implemented

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,93 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import React, { useEffect } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const Search = () => {
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const {
+    data: movies,
+    loading,
+    error,
+    refetch: loadMovies,
+    reset,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
+  useEffect(() => {
+    const func = async () => {
+      if (searchQuery.trim()) {
+        // Refetch movies when search query changes
+        await loadMovies();
+      } else {
+        reset();
+      }
+    };
+    func();
+  }, [searchQuery]);
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        numColumns={3}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        ListHeaderComponent={
+          <>
+            <View className="w-ful flex-row justify-center mt-20 items-center ">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search movies...."
+                value={searchQuery}
+                onChangeText={(text) => setSearchQuery(text)}
+              />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#000fff"
+                className="my-3"
+              />
+            )}
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+            {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
+              <Text className="text-xl text-white font-bold">
+                Search Results for{" "}
+                <Text className="text-accent">SEARCH TERM</Text>
+              </Text>
+            )}
+          </>
+        }
+      />
     </View>
   );
 };
---
+++ services/api.ts
@@ -0,0 +1,42 @@
+export const TMDB_CONFIG = {
+  baseUrl: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endporint = query
+    ? `${TMDB_CONFIG.baseUrl}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.baseUrl}/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(endporint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+  if (!response.ok) {
+    //@ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+
+  const data = await response.json();
+  return data.results;
+};
+
+// const url =
+//   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNGE1ODljNWIzNTJiYzczMGQxMDFmNjkzZWI4M2Q3NCIsIm5iZiI6MTc1MTQ0NDc1NC40MDksInN1YiI6IjY4NjRlZDEyZDdjNzc4ZWVmNDQzMGIyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KIIEBkDzQsfEPufC9za321EY3U-E76NlOQQ-5-dcMFA",
+//   },
+// };
+
+// fetch(url, options)
+//   .then((res) => res.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => console.error(err));
